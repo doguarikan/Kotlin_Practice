cd9999854b72171a29d33179fde24929b5932824
Doğu Arıkan <dogubahaa@gmail.com>
2025-04-27 17:35:26 +0300

    created

---
+++ Subject_One/app/src/main/java/com/example/subject_one/AssignmentVariables.kt
@@ -0,0 +1,97 @@
+package com.example.subject_one
+
+fun main() {
+    val sehir = "İstanbul"
+    val ulke = "Türkiye"
+    val telefon = "+90 555 123 4567"
+    val postaKodu = "34000"
+    val email = "ornek@email.com"
+    val meslek = "Yazılım Geliştirici"
+    val musteriAdi = "Ahmet Yılmaz"
+    val dogumGunu = "15.05.1990"
+    val medeniDurum = "Evli"
+    val maas = 15000.0
+    val bakiye = 5250.75
+    val stokMiktari = 250
+    val urunYorum = "Çok memnun kaldım, tavsiye ederim."
+    val urunAdi = "Akıllı Telefon"
+    val urunPuani = 4.8
+    val indirimMiktari = 500.0
+    val yemekFiyati = 150.0
+    val odemeTarihi = "25.04.2025"
+    val odeme = 2500.0
+    val siparisAdeti = 3
+    val rezervasyonTarihi = "10.05.2025"
+    val takipKodu = "TR123456789"
+    val kuponSuresi = 30
+    val kuponKodu = "BAHAR25"
+    val faturaAdresi = "Atatürk Cad. No:123 Kadıköy/İstanbul"
+    val marka = "Samsung"
+    val arabaModeli = "Hyundai Tucson"
+    val kitapAdi = "Veri Yapıları ve Algoritmalar"
+    val yayinlamaTarihi = "10.01.2023"
+    val muzikAdi = "Beethoven - Für Elise"
+    val videoSuresi = 360
+    val resimAdi = "manzara.jpg"
+    val dosyaFormati = "PDF"
+    val renk = "Mavi"
+    val renkKodu = "#0000FF"
+    val telefonModeli = "iPhone 14 Pro"
+    val ekranBoyutu = 6.1
+    val agirlik = 0.75
+    val ulusalGun = "29 Ekim"
+    val tatilGunu = "Pazar"
+    val sokakAdi = "İstiklal Caddesi"
+    val otobusHatti = "500T"
+    val kalanDakika = 45
+    val odaSayisi = 3
+    val enlem = 41.0082
+    val boylam = 28.9784
+
+    println("Şehir: $sehir")
+    println("Ülke: $ulke")
+    println("Telefon: $telefon")
+    println("Posta Kodu: $postaKodu")
+    println("E-mail: $email")
+    println("Meslek: $meslek")
+    println("Müşteri Adı: $musteriAdi")
+    println("Doğum Günü: $dogumGunu")
+    println("Medeni Durum: $medeniDurum")
+    println("Maaş: $maas TL")
+    println("Bakiye: $bakiye TL")
+    println("Stok Miktarı: $stokMiktari")
+    println("Ürün Yorumu: $urunYorum")
+    println("Ürün Adı: $urunAdi")
+    println("Ürün Puanı: $urunPuani")
+    println("İndirim Miktarı: $indirimMiktari TL")
+    println("Yemek Fiyatı: $yemekFiyati TL")
+    println("Ödeme Tarihi: $odemeTarihi")
+    println("Ödeme: $odeme TL")
+    println("Sipariş Adeti: $siparisAdeti")
+    println("Rezervasyon Tarihi: $rezervasyonTarihi")
+    println("Takip Kodu: $takipKodu")
+    println("Kupon Süresi: $kuponSuresi gün")
+    println("Kupon Kodu: $kuponKodu")
+    println("Fatura Adresi: $faturaAdresi")
+    println("Marka: $marka")
+    println("Araba Modeli: $arabaModeli")
+    println("Kitap Adı: $kitapAdi")
+    println("Yayınlama Tarihi: $yayinlamaTarihi")
+    println("Müzik Adı: $muzikAdi")
+    println("Video Süresi: $videoSuresi saniye")
+    println("Resim Adı: $resimAdi")
+    println("Dosya Formatı: $dosyaFormati")
+    println("Renk: $renk")
+    println("Renk Kodu: $renkKodu")
+    println("Telefon Modeli: $telefonModeli")
+    println("Ekran Boyutu: $ekranBoyutu inç")
+    println("Ağırlık: $agirlik kg")
+    println("Ulusal Gün: $ulusalGun")
+    println("Tatil Günü: $tatilGunu")
+    println("Sokak Adı: $sokakAdi")
+    println("Otobüs Hattı: $otobusHatti")
+    println("Kalan Dakika: $kalanDakika")
+    println("Oda Sayısı: $odaSayisi")
+    println("Enlem: $enlem")
+    println("Boylam: $boylam")
+}
